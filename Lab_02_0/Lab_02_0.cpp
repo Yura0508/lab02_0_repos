a018bb6b4faca73d88e57094c30791daad71613b
Yura <urijkisil2007@gmail.com>
2024-09-19 00:32:10 +0300

    master3 commit

--- Lab_02_0/Lab_02_0.cpp
+++ Lab_02_0/Lab_02_0.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
 	double z1;
-	//double z2;
+	double z2;
 	double a;
 	double b;
 	double PI = 4 * atan(1.0);
@@ -17,10 +17,10 @@ int main()
 	cout << "a = "; cin >> a;
 
 	z1 = pow(cos(PI * 3 / 8 - a / 4), 2) - pow(cos((PI * 11 / 8) + a / 4), 2);
-	//z2 = sqrt(2) / 2 * sin(a / 2);
+	z2 = sqrt(2) / 2 * sin(a / 2);
 
 	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	cout << "z2 = " << z2 << endl;
 	cin.get();
 
 	return 0;
